e87294d26822e584521e5b86339581f034359c97
Brian Truong <briantruong777@gmail.com>
2014-10-21 18:07:29 -0400

    I don't know english

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is a spotpost!'
+    return 'This is spotpost!'
 
 if __name__ == '__main__':
   # Runs on port 5000 by default
