c82a517e40e4e01b4586abdfda2cfad86eb97361
Brian Truong <briantruong777@gmail.com>
2014-10-21 17:34:16 -0400

    added main.py file

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'This is a spotpost!'
+
+if __name__ == '__main__':
+  # Runs on port 5000 by default
+  # url: "localhost:5000"
+  app.run(host="0.0.0.0")
