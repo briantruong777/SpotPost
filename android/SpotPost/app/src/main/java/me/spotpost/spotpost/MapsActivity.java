6f5aad17e308919378d53f67d50f9abeaf1783b3
Brian Truong <briantruong777@gmail.com>
2014-11-10 16:41:02 -0600

    allowed find location in map app

--- android/SpotPost/app/src/main/java/me/spotpost/spotpost/MapsActivity.java
+++ android/SpotPost/app/src/main/java/me/spotpost/spotpost/MapsActivity.java
@@ -68,5 +68,6 @@ public class MapsActivity extends FragmentActivity
     private void setUpMap()
     {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
     }
 }
