aa6f858f89139bcf086833d3e1d92d44841eafb1
Nate Norgaard <norgaar2@illinois.edu>
2014-11-20 15:34:27 -0600

    added debug prints

--- static/js/app.js
+++ static/js/app.js
@@ -21,10 +21,14 @@ var renderUserPage = function() {
   );
 };
 
+console.log(document.getElementById('app'));
+console.log(document.getElementById('loginApp'));
 if (document.getElementById('app')) {
+  console.log('USER PAGE');
   renderUserPage();
 }
 else if (document.getElementById('loginApp')) {
+  console.log('LOGIN PAGE');
   renderLoginPage();
 }
 
