27283e1d2c6b7d48ed0f87930d55294b3266cbff
wlodarczykj <wlodarczykj1027@gmail.com>
2014-11-19 22:32:56 -0600

    Fixed reroute on login
    Reroutes you to index now.

--- main.py
+++ main.py
@@ -370,7 +370,7 @@ def index():
 	#if 'username' in session:
 	#	return 'Logged in as %s' % escape(session['username'])
 	#return 'You are not logged in'
-	if 'username' in session.keys():
+	if 'username' in session:
 		return render_template('index.html')
 	else:
 		return redirect(url_for('login'))
