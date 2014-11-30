2e3464c0517081ceb41135a458d7188eb588496c
Brian Truong <briantruong777@gmail.com>
2014-11-30 00:22:27 -0600

    Improved loading animation because I felt like wasting time

--- android/SpotPost/app/src/main/java/me/spotpost/spotpost/LoginActivity.java
+++ android/SpotPost/app/src/main/java/me/spotpost/spotpost/LoginActivity.java
@@ -1,19 +1,22 @@
 package me.spotpost.spotpost;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewTreeObserver;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.util.Log;
 
@@ -70,7 +73,38 @@ public class LoginActivity extends Activity
         });
 
         mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+
+        // create new ProgressBar and style it
+        final ProgressBar progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
+        progressBar.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, 36));
+        progressBar.setIndeterminate(true);
+        progressBar.setVisibility(View.INVISIBLE);
+        mProgressView = progressBar;
+
+        // retrieve the top view of our application
+        final FrameLayout decorView = (FrameLayout) getWindow().getDecorView();
+        decorView.addView(progressBar);
+
+        // Here we try to position the ProgressBar to the correct position by looking
+        // at the position where content area starts. But during creating time, sizes
+        // of the components are not set yet, so we have to wait until the components
+        // has been laid out
+        // Also note that doing progressBar.setY(136) will not work, because of different
+        // screen densities and different sizes of actionBar
+        ViewTreeObserver observer = progressBar.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                View contentView = decorView.findViewById(android.R.id.content);
+                progressBar.setY(contentView.getY() - 20);
+
+                ViewTreeObserver observer = progressBar.getViewTreeObserver();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                    observer.removeOnGlobalLayoutListener(this);
+                else
+                    observer.removeGlobalOnLayoutListener(this);
+            }
+        });
     }
 
     /**
@@ -115,8 +149,6 @@ public class LoginActivity extends Activity
         }
         else
         {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
             ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected())
@@ -163,27 +195,7 @@ public class LoginActivity extends Activity
     {
         int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-        mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                show ? 0 : 1).setListener(new AnimatorListenerAdapter()
-        {
-            @Override
-            public void onAnimationEnd(Animator animation)
-            {
-                mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            }
-        });
-
-        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-        mProgressView.animate().setDuration(shortAnimTime).alpha(
-                show ? 1 : 0).setListener(new AnimatorListenerAdapter()
-        {
-            @Override
-            public void onAnimationEnd(Animator animation)
-            {
-                mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            }
-        });
+        mProgressView.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
     }
 }
 
