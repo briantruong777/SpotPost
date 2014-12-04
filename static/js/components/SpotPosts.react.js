37175367afc039ece4c63a02c2a6832e25484561
Nate Norgaard <norgaar2@illinois.edu>
2014-12-04 13:06:58 -0600

    fixed unknown reference error

--- static/js/components/SpotPosts.react.js
+++ static/js/components/SpotPosts.react.js
@@ -7,18 +7,23 @@ var SpotPost = require('./SpotPost.react');
 
 var SpotPosts = React.createClass({
 
+  var passInfo = {
+    editId: -1,
+    isLoading: false
+  }
+
   _renderSpotPost: function(spotPost) {
     var edit = false;
-    if (spotPost.id === editId) {
+    if (spotPost.id === passInfo.editId) {
       edit = true;
     }
-    return (<li><UserSpotPost spotPost={spotPost} edit={edit} isLoading={isLoading} /></li>);
+    return (<li><UserSpotPost spotPost={spotPost} edit={edit} isLoading={passInfo.isLoading} /></li>);
   },
 
   render: function() {
     var spotPosts = this.props.spotPosts;
-    var isLoading = this.props.isLoading;
-    var editId = this.props.editId;
+    passInfo.isLoading = this.props.isLoading;
+    passInfo.editId = this.props.editId;
     
     if (spotPosts === undefined) {
       return (<ul><li>No SpotPosts to display.</li></ul>);
